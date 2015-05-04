f40c9cf980f5f5ff0ace2c94f6f0b3f193d8564a
SeagullTown <bjornmake@gmail.com>
2015-05-04 19:39:54 +0200

    Frame up and running

--- src/view/Ages.java
+++ src/view/Ages.java
@@ -1,14 +1,64 @@
 package view;
 
-import javax.swing.SwingUtilities;
+import java.awt.Canvas;
+import java.awt.Dimension;
 
-public class Ages {
+import javax.swing.JFrame;
 
-	public static void main(String[] args) {
-		
-		
-		
 
+public class Ages extends Canvas implements Runnable{
+	
+	public static int width = 300;
+	public static int height = width / 16*9; 
+	public static int scale = 3;
+	
+	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
+
+	public Ages() {
+		Dimension size = new Dimension(width * scale ,height * scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
 	}
+		
+	public synchronized void start() {
+		running = true;
+		thread = new Thread(this, "Display");
+		thread.start();
+		
+	}
+	
+	public synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		Ages ages = new Ages();
+		ages.frame.setResizable(false);
+		ages.frame.setTitle("Ages");
+		ages.frame.add(ages);
+		ages.frame.pack();
+		ages.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ages.frame.setLocationRelativeTo(null);
+		ages.frame.setVisible(true);
+	}
+	
+	/*
+	 * game loop
+	 */
+	public void run() {
+		while(running) {
+			
+		}
+	}
+
+	
 
 }

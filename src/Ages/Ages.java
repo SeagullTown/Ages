948f103931ce35ef2b4c8b775d185f52ebb0d92c
SeagullTown <bjornmake@gmail.com>
2015-05-07 00:09:10 +0200

    A lot

--- src/Ages/Ages.java
+++ src/Ages/Ages.java
@@ -1,9 +1,6 @@
 package Ages;
 
-import graphics.Screen;
-
 import java.awt.Canvas;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
@@ -12,40 +9,45 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import Ages.graphics.Screen;
+import Ages.input.Keyboard;
 
-public class Ages extends Canvas implements Runnable{
-	
-	
+public class Ages extends Canvas implements Runnable {
+
 	private static final long serialVersionUID = 1L;
 	public static int width = 300;
-	public static int height = width / 16*9; 
+	public static int height = width / 16 * 9;
 	public static int scale = 3;
-	
+	public static String title = ("Ages");
+
 	private Thread thread;
 	private JFrame frame;
+	private Keyboard key;
 	private boolean running = false;
-	
-	private BufferedImage image = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
-	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
-	
+
 	private Screen screen;
 
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
 	public Ages() {
-		Dimension size = new Dimension(width * scale ,height * scale);
+		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
-		screen = new Screen(width,height);
-		
-		
+		screen = new Screen(width, height);
+
+		key = new Keyboard();
+		addKeyListener(key);
+
 		frame = new JFrame();
 	}
-		
+
 	public synchronized void start() {
 		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
-		
+
 	}
-	
+
 	public synchronized void stop() {
 		running = false;
 		try {
@@ -54,7 +56,7 @@ public class Ages extends Canvas implements Runnable{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Ages ages = new Ages();
 		ages.frame.setResizable(false);
@@ -64,46 +66,78 @@ public class Ages extends Canvas implements Runnable{
 		ages.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ages.frame.setLocationRelativeTo(null);
 		ages.frame.setVisible(true);
-		
+
 		ages.start();
 	}
-	
+
 	/*
 	 * game loop
 	 */
 	public void run() {
-		while(running) {
-			update();
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
+		requestFocus();
+
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+
+			while (delta >= 1) {
+				update();
+				updates++;
+				delta--;
+			}
+
 			render();
+			frames++;
+
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				// System.out.println("fps: " + frames + ", updates: " +
+				// updates);
+				frame.setTitle(title + "   |   " + frames + " fps | " + updates + " ups");
+				updates = 0;
+				frames = 0;
+			}
 		}
+		stop();
+
 	}
-	
+
+	int x = 0, y = 0;
+
 	public void update() {
-		
+
+		key.update();
+		if (key.up) y--;
+		if (key.down) y++;
+		if (key.left) x--;
+		if (key.right) x++;
+
 	}
-	
+
 	public void render() {
 		BufferStrategy bs = getBufferStrategy();
-		if(bs == null) {
+		if (bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
 		screen.clear();
-		screen.render();
-		
-		for(int i=0;i<pixels.length;i++) {
+		screen.render(x, y);
+
+		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = screen.pixels[i];
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
-		g.setColor(Color.black);
-		g.fillRect(0, 0, getWidth(), getHeight());
-		g.drawImage(image, 0, 0, getWidth(), getHeight(),null);
-		
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
 		bs.show();
 	}
-
-	
 
 }
---
+++ src/Ages/graphics/Sprite.java
@@ -0,0 +1,36 @@
+package Ages.graphics;
+
+/*
+ * Spire class for each sprite in the game, handles different sizes.
+ */
+
+public class Sprite {
+
+	public final int SIZE;
+	private int x, y;
+	private SpriteSheet sheet;
+	public int[] pixels;
+
+	public static Sprite grass = new Sprite(16, 0, 0, SpriteSheet.tiles);
+
+	public Sprite(int size, int x, int y, SpriteSheet sheet) {
+		SIZE = size;
+		pixels = new int[SIZE * SIZE];
+		this.x = x * size;
+		this.y = y * size;
+		this.sheet = sheet;
+		load();
+
+	}
+
+	private void load() {
+
+		for (int y = 0; y < SIZE; y++) {
+			for (int x = 0; x < SIZE; x++) {
+				pixels[x + y * SIZE] = sheet.pixels[(x + this.x) + (y + this.y)
+						* sheet.SIZE];
+			}
+		}
+	}
+
+}
